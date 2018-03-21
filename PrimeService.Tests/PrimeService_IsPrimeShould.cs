b446410d0d3232100edd06ce0f3b6b728fd8bbd3
Shayne Boyer <spboyer@live.com>
2018-03-21 09:03:51 -0400

    init

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+  [TestClass]
+  public class PrimeService_IsPrimeShould
+  {
+    private readonly PrimeService _primeService;
+
+    public PrimeService_IsPrimeShould()
+    {
+      _primeService = new PrimeService();
+    }
+
+    [TestMethod]
+    public void PrimeService_CanBeCreated()
+    {
+      Assert.IsNotNull(_primeService, "PrimeService can be created.");
+    }
+
+    [TestMethod]
+    public void ReturnFalseGivenValueOf1()
+    {
+      var result = _primeService.IsPrime(1);
+
+      Assert.IsFalse(result, "1 should not be prime");
+    }
+
+    [DataTestMethod]
+    [DataRow(-1)]
+    [DataRow(0)]
+    [DataRow(1)]
+    public void ReturnFalseGivenValuesLessThan2(int value)
+    {
+      var result = _primeService.IsPrime(value);
+
+      Assert.IsFalse(result, $"{value} should not be prime");
+    }
+  }
+}
